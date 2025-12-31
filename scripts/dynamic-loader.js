95e08f35ddccefdf651a7f0723052f10c0804160
The Pantry Forge <thepantryforge@gmail.com>
2025-12-31 16:11:28 +1100

    Add favicons
    Added a new maskable icon image and updated dynamic-loader.js to include favicon and apple-touch-icon links.

--- scripts/dynamic-loader.js
+++ scripts/dynamic-loader.js
@@ -7,6 +7,11 @@ head.innerHTML += `\
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<!-- Icons -->
+<link rel="icon" type="image/png" href="images/icon.png"/>
+<link rel="icon" type="image/png" href="images/icon-maskable.png"/>
+<link rel="apple-touch-icon" type="image/png" href="images/icon-maskable.png"/>
+
 <!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
